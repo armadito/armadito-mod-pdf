93d3311cb5742f4089c7049cc0bc2b7286a69d48
Ulrich <ufausther@teclib.com>
2016-06-01 17:55:26 +0200

    update license header

--- module7/main.c
+++ module7/main.c
@@ -1,33 +1,37 @@
-/*  
-	< ARMADITO PDF ANALYZER is a tool to parses and analyze PDF files in order to detect potentially dangerous contents.>
-    Copyright (C) 2015 by Ulrich FAUSTHER <ufausther@teclib.com>
-    
+/***
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+Copyright (C) 2015, 2016 Teclib'
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This file is part of Armadito module PDF.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Armadito module PDF is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-*/
+Armadito module PDF is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Armadito module PDF.  If not, see <http://www.gnu.org/licenses/>.
+
+***/
+
+
     
 #include "pdfAnalyzer.h"
 
 
 void Helper(){
 	
-	printf("ARMADITO PDF ANALYZER :: No file in parameter\n");
+	printf("ARMADITO PDF ANALYZER :: invalid parameter\n");
 	printf("Command : ./pdfAnalyzer [filename]\n\n");
 
 	return;
 }
+
 
 void Banner(){
 	
@@ -42,31 +46,26 @@ void Banner(){
 int main (int argc, char ** argv){
 
 	int ret;
-	//FILE * f = NULL;
-	//int fd = -1;
-	
-	#ifdef DEBUG
-	Banner();
-	#endif
+	FILE * f = NULL;
+	int fd = -1;
+		
+	Banner();	
 
 	if(argc < 2){
 		Helper();
 		return (-1);
 	}
 	
-#if 0
+#if 1
 
-	// Code for testing analysis with file descriptor.
-	//printf ("Analyzing file : %s\n",argv[1]);
+	// analysis with opened file descriptor.	
 	if(!(f = os_fopen(argv[1],"rb"))){
-		printf("[-] Error :: main :: Error while opening file %s\n",argv[1]);
+		err_log("Can't open file %s\n", argv[1]);
 		return -1;
 	}
 	
 	fd = os_fileno(f);
-	//printf("[+] Debug :: fd = %d\n",fd);
-	ret = analyzePDF_fd(fd, argv[1]);
-
+	ret = analyzePDF_ex(fd, argv[1]);
 	fclose(f);
 
 #else
@@ -75,7 +74,7 @@ int main (int argc, char ** argv){
 	
 #endif	
 
-	//system("pause");
+	system("pause");
 	
 	return ret;
 }
--- module7/parser.c
+++ module7/parser.c
@@ -1,22 +1,25 @@
-/*  
-	< ARMADITO PDF ANALYZER is a tool to parses and analyze PDF files in order to detect potentially dangerous contents.>
-    Copyright (C) 2015 by Ulrich FAUSTHER <ufausther@teclib.com>
-    
+/***
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+Copyright (C) 2015, 2016 Teclib'
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This file is part of Armadito module PDF.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Armadito module PDF is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-*/
+Armadito module PDF is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Armadito module PDF.  If not, see <http://www.gnu.org/licenses/>.
+
+***/
+
+
     
 #include "pdfAnalyzer.h"
 
