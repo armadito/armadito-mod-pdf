8ee6251807e3db8a71ea72127b20e83039272b84
Ulrich FAUSTHER <ufausther@teclib.com>
2017-02-20 13:53:05 +0100

    Add getVersion() function

--- lib/includes/armaditopdf.h
+++ lib/includes/armaditopdf.h
@@ -69,12 +69,13 @@ enum err_code {
 
 
 /* Functions */
-void Helper();
+char * getVersion();
 int analyzePDF(char * filename);
 int analyzePDF_fd(int fd, char * filename);
 int analyzePDF_ex(int fd, char * filename);
 int calcSuspiciousCoefficient(struct pdfDocument * pdf);
 void printAnalysisReport(struct pdfDocument * pdf);
+
 
 
 #endif
